916061e981cd2a86bd5331cb18ca10f55149c24f
sidhantnair <nair.sidhant@gmail.com>
2023-04-23 01:34:51 -0400

    Initial Commit

---
+++ BionicWikipedia/BionicWikipediaApp.swift
@@ -0,0 +1,17 @@
+//
+//  BionicWikipediaApp.swift
+//  BionicWikipedia
+//
+//  Created by Sidhant Nair on 4/23/23.
+//
+
+import SwiftUI
+
+@main
+struct BionicWikipediaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
